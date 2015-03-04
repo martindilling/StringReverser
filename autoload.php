85ac147285886677cd28b2f2423cc35c00abe0f9
Martin Dilling-Hansen <martindilling@gmail.com>
2015-03-04 19:51:35 +0100

    Simple project specific autoloader

---
+++ autoload.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Project-specific implementation.
+ *
+ * https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+ *
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
+spl_autoload_register(function ($class) {
+
+    // Namespace prefix
+    $prefix = 'Reverser\\';
+
+    // Base directory
+    $base_dir = __DIR__ . '/src/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
